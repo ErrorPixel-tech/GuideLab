c1c12bc82e14668e58d9f178aeb0bfd47fc45928
German <fedosihingerman@yandex.ru>
2026-02-09 00:02:02 +0300

    link fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './buttons.scss';
 function App() {
   return (
     <div className="app">
-      <a href='https://steamcommunity.com/sharedfiles/filedetails/?id=3662682549' className='help-button'>?</a>
+      <a target="_blank" href='https://steamcommunity.com/sharedfiles/filedetails/?id=3662682549' className='help-button'>?</a>
       <ButtonColumn />
       <InputColumn />
       <MarkupColumn />
