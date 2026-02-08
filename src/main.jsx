098736c649b8ce7f2620573a81331567d9ae47ee
German <fedosihingerman@yandex.ru>
2026-02-08 17:53:02 +0300

    translate works!!!

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux';
 import { store } from './store/store.js';
-// import "./i18n";
 import "./i18n";
 
 createRoot(document.getElementById('root')).render(
